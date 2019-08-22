3a717824e3a211647d1ba34e684624e12690a1cb
Swizec Teller <swizec@swizec.com>
2019-08-22 00:14:44 -0700

    Fix data import

--- dist/BigMacIndex.d.ts
+++ dist/BigMacIndex.d.ts
@@ -56,17 +56,3 @@ export declare const BigMacIndex: {
     Ukraine: number;
     Russia: number;
 };
-export default class ParityPrice {
-    ipstack_key: string;
-    cache: {
-        [key: string]: any;
-    };
-    constructor(ipstack_key: string);
-    private ipstack;
-    private _price;
-    price(USAprice: number, IP?: string): Promise<number>;
-    priceWithLocation(USAprice: number, IP?: string): Promise<{
-        fairPrice: number;
-        location: any;
-    }>;
-}
